a8e52c5b9d0a1fd80df58e32fcc9c5795396f782
Cruz Sanchez <cruzsanchez9209@gmail.com>
2020-04-11 00:56:34 -0500

    Added Person.cs, 90% complete I think...

--- BankingConsoleUI/Program.cs
+++ BankingConsoleUI/Program.cs
@@ -6,7 +6,14 @@ namespace BankingConsoleUI
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Gemmie ur name");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("What is your social? No Hyphens digits only");
+            string userSSN = Console.ReadLine();
+
+            Person p = new Person(userName, userSSN);
+            p.DisplayUserInfo();
         }
     }
 }
